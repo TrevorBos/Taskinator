320c258319de80bba335573edf6bf0f1ea310897
Trevor <trevor.bos@outlook.com>
2022-05-24 10:36:50 -0400

    Made it so that the browser wouldnt reload itself whenever the user put in a new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 // Global variables
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector ("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //function that creates a new task, gets called in the eventListener below.
-var createTaskHandler = function () {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task!";
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task!";
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 // Calls the above function to create a new task when the user clicks the button.
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
