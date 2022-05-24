945fb575cbdf6cb8b4393f0a5673c9e8a9270f7a
Trevor <trevor.bos@outlook.com>
2022-05-24 11:09:44 -0400

    Added some refactoring to the JS file to make the code more easily readable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,39 @@ var formEl = document.querySelector ("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //function that creates a new task, gets called in the eventListener below.
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
 
     event.preventDefault();
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task!";
-    tasksToDoEl.appendChild(listItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //package the data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+
 };
 
-// Calls the above function to create a new task when the user clicks the button.
-formEl.addEventListener("submit", createTaskHandler);
+var createTaskEl = function (taskDataObj){
+    // Create the list items
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create the div to hold the task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className - "task-info";
+    // add HTML content to the div
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add the entire list item to the list
+    tasksToDoEl.appendChild(listItemEl);
+
+}
+
+formEl.addEventListener("submit", taskFormHandler);
